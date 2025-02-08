46248cef8e5ebf2304421c4ae9fef3c86ae99468
0xLab <github.0xlab@gmail.com>
2025-02-08 16:01:24 +0300

    crush bugfix

--- app/src/main/java/com/monistoWallet/additional_wallet0x/no_account/root/ui/presentation/AccountNotFound.kt
+++ app/src/main/java/com/monistoWallet/additional_wallet0x/no_account/root/ui/presentation/AccountNotFound.kt
@@ -30,7 +30,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.glance.LocalContext
+import androidx.compose.ui.platform.LocalContext
 import com.monistoWallet.R
 import com.monistoWallet.additional_wallet0x.root.model.VerificationSuccessModel
 import com.monistoWallet.additional_wallet0x.no_account.login_email_verification.ui.presentation.VerificationLoginScreen
